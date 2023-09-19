e8d65461d43bc25b8d7782477f940f90921fa3cb
Kun Chen <chenkun0228@gmail.com>
2023-09-19 15:04:28 -0400

    add support for 2d layered ueg

--- example/freeE/freeE_MC.jl
+++ example/freeE/freeE_MC.jl
@@ -3,17 +3,18 @@ using JLD2
 
 dim = 2
 rs = [1.0]
-mass2 = [0.001, 1.0, 2.0]
+mass2 = [1.0,]
 # mass2 = [0.5, 1.0, 2.0, 4.0]
 # mass2 = [0.001]
 Fs = [-0.0,]
 # beta = [25.0, 40.0, 80.0]
 beta = [80.0]
 order = [0,]
-neval = 1e8
-isDynamic = false
-isFock = false
+neval = 1e6
+isDynamic = false # dynamically screened Coulomb interaction or not
+isFock = false # Fock renormalized Green's function or not
 spinPolarPara = 0.0 # spin-polarization parameter (n_up - n_down) / (n_up + n_down) ∈ [0,1]
+isLayered2D = false
 
 for (_rs, _mass2, _F, _beta, _order) in Iterators.product(rs, mass2, Fs, beta, order)
     para = UEG.ParaMC(rs=_rs, beta=_beta, Fs=_F, order=_order, mass2=_mass2, isDynamic=isDynamic, dim=dim, isFock=isFock)
@@ -21,6 +22,8 @@ for (_rs, _mass2, _F, _beta, _order) in Iterators.product(rs, mass2, Fs, beta, o
     filename = "data_freeE_test.jld2"
 
     # freeE, result = FreeEnergy.MC(para; neval=neval, filename=filename, spinPolarPara=spinPolarPara)
-    freeE, result = FreeEnergy.MC(para; neval=neval, filename=filename,
+    freeE, result = FreeEnergy.MC(para;
+        isScreened=isLayered2D,
+        neval=neval, filename=filename,
         spinPolarPara=spinPolarPara, partition=[(1, 0, 0)])
 end
