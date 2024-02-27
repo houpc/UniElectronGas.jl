9c007c3de53fe1672c5bfdc6c7968e2304d70857
dcerkoney <dcerkoney@gmail.com>
2024-02-27 11:19:21 -0500

    Bugfix for KO

--- example/sigma/get_meff_gw.jl
+++ example/sigma/get_meff_gw.jl
@@ -109,7 +109,6 @@ function get_Σ_GW(param::Parameter.Para, int_type, max_steps, atol, alpha, δK,
 
     # Make sure we are using parameters for the bare UEG theory
     @assert param.Λs == param.Λa == 0.0
-    @assert int_type == :rpa
 
     # Get Fermi liquid parameter F⁰ₛ(rs) from Perdew-Wang fit
     rs = round(param.rs; sigdigits=13)
@@ -144,7 +143,7 @@ function get_Σ_GW(param::Parameter.Para, int_type, max_steps, atol, alpha, δK,
             minK=minK,
             order=order,
             int_type=_int_type,
-            Fs=int_type == :ko_const_p ? -Fs : -0.0,   # NOTE: NEFT uses opposite sign convention for F!
+            Fs=_int_type == :ko_const ? -Fs : -0.0,   # NOTE: NEFT uses opposite sign convention for F!
             Fa=int_type == :ko_const_pm ? -Fa : -0.0,  # NOTE: NEFT uses opposite sign convention for F!
         )
     end
@@ -265,7 +264,7 @@ function get_meff_from_Σ_G0W(param::Parameter.Para; δK=5e-6, int_type=:rpa)
         minK=minK,
         order=order,
         int_type=_int_type,
-        Fs=int_type == :ko_const_p ? -Fs : -0.0,   # NOTE: NEFT uses opposite sign convention for F!
+        Fs=_int_type == :ko_const ? -Fs : -0.0,   # NOTE: NEFT uses opposite sign convention for F!
         Fa=int_type == :ko_const_pm ? -Fa : -0.0,  # NOTE: NEFT uses opposite sign convention for F!
     )
 
